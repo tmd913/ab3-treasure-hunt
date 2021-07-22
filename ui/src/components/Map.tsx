571292344b1649bc424618bb5eb9b4978b340602
tom dennis <tomdenn@amazon.com>
2021-07-22 17:24:31 -0400

    map enhancements

--- ui/src/components/Map.tsx
+++ ui/src/components/Map.tsx
@@ -10,6 +10,9 @@ import ReactMapGL, {
   NavigationControl,
   ViewportProps,
   Marker,
+  Layer,
+  Source,
+  LayerProps,
 } from 'react-map-gl';
 import amplifyConfig from '../amplify-config';
 import { environment } from '../environment';
@@ -17,6 +20,8 @@ import { useAuth } from '../auth/use-auth';
 import { Button, createStyles, makeStyles, Theme } from '@material-ui/core';
 import './Map.css';
 import Pin from './Pin';
+import HomeIcon from '@material-ui/icons/Home';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -31,14 +36,17 @@ const useStyles = makeStyles((theme: Theme) =>
         backgroundColor: theme.palette.grey[200],
       },
     },
-    homeButton: {
-      minWidth: 40,
-      width: 40,
-      height: 40,
+    homeIcon: {
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      borderRadius: 4,
+    },
+    arrowIcon: {
+      width: 30,
+      height: 30,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
       borderRadius: '50%',
-      position: 'absolute',
-      top: 20,
-      right: 20,
     },
   })
 );
@@ -46,13 +54,32 @@ const useStyles = makeStyles((theme: Theme) =>
 const geolocateStyle = {
   top: 100,
 };
+
+const layerStyle: LayerProps = {
+  id: 'route',
+  type: 'line',
+  layout: {
+    'line-join': 'round',
+    'line-cap': 'round',
+  },
+  paint: {
+    'line-color': '#007cbf',
+    'line-width': 8,
+  },
+};
+
 const positionOptions = { enableHighAccuracy: true };
 const flyToInterpolator = new FlyToInterpolator();
+
+const BASE_ROTATION = 360 - 112.5;
 
 const Map = ({
   onMapClick,
   marker,
   playerLocation,
+  geojson,
+  handleLocationChange,
+  createMockLocation,
 }: {
   onMapClick?: Function;
   marker?: {
@@ -63,6 +90,9 @@ const Map = ({
     latitude: number;
     longitude: number;
   };
+  geojson?: GeoJSON.Feature<GeoJSON.LineString, GeoJSON.GeoJsonProperties>;
+  handleLocationChange?: Function;
+  createMockLocation?: Function;
 }) => {
   const auth = useAuth();
 
@@ -76,10 +106,35 @@ const Map = ({
     zoom: 12,
   });
 
-  const [rotation, setRotation] = useState<number>(0);
+  const [heading, setHeading] = useState<number>();
+  const [rotation, setRotation] = useState<number>(BASE_ROTATION);
   const [dot, setDot] = useState<Element>();
+  const [current, setCurrent] = useState<Element>();
   const [counter, setCounter] = useState<number>(0);
   const [isAdmin, setIsAdmin] = useState<boolean>();
+  const [data, setData] =
+    useState<GeoJSON.Feature<GeoJSON.LineString, GeoJSON.GeoJsonProperties>>();
+
+  useEffect(() => {
+    if (!geojson) {
+      return;
+    }
+
+    const replacement: GeoJSON.Feature<
+      GeoJSON.LineString,
+      GeoJSON.GeoJsonProperties
+    > = {
+      ...geojson,
+      ...{
+        geometry: {
+          type: 'LineString',
+          coordinates: [...geojson?.geometry.coordinates] || [],
+        },
+      },
+    };
+
+    setData(Object.assign({}, replacement));
+  }, [geojson]);
 
   useEffect(() => {
     setIsAdmin(auth.userGroups?.includes('Admins'));
@@ -102,32 +157,68 @@ const Map = ({
     makeRequestTransformer();
   }, [auth]);
 
+  // useEffect(() => {
+  //   if (dot) {
+  //     const div = (
+  //       <div className="radar-container">
+  //         <svg
+  //           className="radar"
+  //           height="20"
+  //           width="20"
+  //           viewBox="0 0 20 20"
+  //           transform={`rotate(${rotation})`}
+  //         >
+  //           <circle
+  //             r="5"
+  //             cx="50%"
+  //             cy="50%"
+  //             fill="transparent"
+  //             stroke="rgba(235, 29, 29, 0.66)"
+  //             strokeWidth="10"
+  //             strokeDasharray="3.925 27.475"
+  //           />
+  //         </svg>
+  //       </div>
+  //     );
+  //     ReactDOM.render(div, dot);
+  //   }
+  // }, [dot, rotation]);
+
+  // useEffect(() => {
+  //   if (current) {
+  //     const div = (
+  //       <div className="radar-container">
+  //         <svg
+  //           className="radar"
+  //           height="20"
+  //           width="20"
+  //           viewBox="0 0 20 20"
+  //           transform={`rotate(${rotation})`}
+  //         >
+  //           <circle
+  //             r="5"
+  //             cx="50%"
+  //             cy="50%"
+  //             fill="transparent"
+  //             stroke="rgba(235, 29, 29, 0.66)"
+  //             strokeWidth="10"
+  //             strokeDasharray="3.925 27.475"
+  //           />
+  //         </svg>
+  //       </div>
+  //     );
+  //     ReactDOM.render(div, current);
+  //   }
+  // }, [current, rotation]);
+
   useEffect(() => {
-    if (dot) {
-      const div = (
-        <div className="radar-container">
-          <svg
-            className="radar"
-            height="20"
-            width="20"
-            viewBox="0 0 20 20"
-            transform={`rotate(${rotation})`}
-          >
-            <circle
-              r="5"
-              cx="50%"
-              cy="50%"
-              fill="transparent"
-              stroke="rgba(235, 29, 29, 0.66)"
-              strokeWidth="10"
-              strokeDasharray="3.925 27.475"
-            />
-          </svg>
-        </div>
+    document
+      .querySelector('#arrowIcon')
+      ?.setAttribute(
+        'style',
+        `transform: rotate(${rotation - BASE_ROTATION}deg)`
       );
-      ReactDOM.render(div, dot);
-    }
-  }, [dot, rotation]);
+  }, [current, rotation]);
 
   const isMapViewable = (groups?: string[]): boolean => {
     if (!groups || groups.length === 0) {
@@ -158,6 +249,37 @@ const Map = ({
     });
   };
 
+  interface Location {
+    latitude: number;
+    longitude: number;
+  }
+
+  /**
+   * Converts value from degrees to radians
+   * @param deg Value in degrees
+   * @returns Value in radians
+   */
+  const degToRad = (deg: number) => (deg * Math.PI) / 180;
+
+  const calculateBearing = (
+    startLocation: Location,
+    endLocation: Location
+  ): number => {
+    const startLat = degToRad(endLocation.latitude);
+    const startLong = degToRad(endLocation.longitude);
+    const endLat = degToRad(startLocation.latitude);
+    const endLong = degToRad(startLocation.longitude);
+    const y = Math.sin(endLong - startLong) * Math.cos(endLat);
+    const x =
+      Math.cos(startLat) * Math.sin(endLat) -
+      Math.sin(startLat) * Math.cos(endLat) * Math.cos(endLong - startLong);
+    const initialBearingRad = Math.atan2(y, x);
+    const initialBearing = ((initialBearingRad * 180) / Math.PI + 360) % 360; // in degrees
+    const finalBearing = (initialBearing + 180) % 360;
+
+    return Math.round(finalBearing);
+  };
+
   return (
     <div className={classes.mapContainer}>
       {isMapViewable(auth.userGroups) && transformRequest && (
@@ -171,12 +293,24 @@ const Map = ({
             onViewportChange={(viewstate: ViewState) => {
               setViewport(viewstate);
 
-              if (!isAdmin) {
-                setCounter(counter + 1);
-                if (counter % 10 === 0) {
-                  setRotation(rotation + 10);
-                }
-              }
+              // if (!isAdmin) {
+              //   setCounter(counter + 1);
+              //   if (counter % 10 === 0) {
+              //     setRotation(rotation + 10);
+              //   }
+              // }
+              // console.log(viewstate.bearing);
+
+              // setRotation(
+              //   viewstate.bearing
+              //     ? BASE_ROTATION - viewstate.bearing
+              //     : BASE_ROTATION
+              // );
+              setRotation(
+                heading
+                  ? (BASE_ROTATION - (viewstate?.bearing || 0) + heading) % 360
+                  : BASE_ROTATION
+              );
             }}
             onClick={(e: any) => {
               if (onMapClick) {
@@ -184,6 +318,37 @@ const Map = ({
               }
             }}
           >
+            <Source id="my-data" type="geojson" data={data}>
+              <Layer {...layerStyle} source="my-data" />
+            </Source>
+
+            <Marker
+              longitude={
+                geojson?.geometry.coordinates[
+                  geojson.geometry.coordinates.length - 1
+                ][0] || 0
+              }
+              latitude={
+                geojson?.geometry.coordinates[
+                  geojson.geometry.coordinates.length - 1
+                ][1] || 0
+              }
+              offsetTop={-20}
+              offsetLeft={-10}
+            >
+              <div className="current"></div>
+              <ArrowUpwardIcon id="arrowIcon" className={classes.arrowIcon} />
+            </Marker>
+
+            <Marker
+              longitude={geojson?.geometry.coordinates[0][0] || 0}
+              latitude={geojson?.geometry.coordinates[0][1] || 0}
+              offsetTop={-20}
+              offsetLeft={-10}
+            >
+              <HomeIcon className={classes.homeIcon} />
+            </Marker>
+
             {marker?.latitude && marker?.longitude && (
               <Marker
                 longitude={marker.longitude}
@@ -194,13 +359,14 @@ const Map = ({
                 <Pin size={20} />
               </Marker>
             )}
-            <div style={{ position: 'absolute', left: 20, top: 20 }}>
+
+            <div style={{ position: 'absolute', left: 25, top: 80 }}>
               <NavigationControl showCompass={true} />
               <GeolocateControl
                 style={geolocateStyle}
                 positionOptions={positionOptions}
                 showAccuracyCircle={false}
-                trackUserLocation={false}
+                trackUserLocation={true}
                 auto={!isAdmin}
                 onViewportChange={(viewstate: ViewState) => {
                   if (isAdmin) {
@@ -208,19 +374,67 @@ const Map = ({
                     return;
                   }
 
-                  if (!dot) {
-                    const dotEl = document.querySelector(
-                      '.mapboxgl-user-location-dot.mapboxgl-marker.mapboxgl-marker-anchor-center'
-                    );
-                    if (dotEl) {
-                      dotEl.setAttribute('style', 'text-content: center');
-                      setDot(dotEl);
+                  // if (!dot) {
+                  //   const dotEl = document.querySelector(
+                  //     '.mapboxgl-user-location-dot.mapboxgl-marker.mapboxgl-marker-anchor-center'
+                  //   );
+                  //   if (dotEl) {
+                  //     dotEl.setAttribute('style', 'text-content: center');
+                  //     setDot(dotEl);
+                  //   }
+                  // }
+
+                  if (!current) {
+                    const currentEl = document.querySelector('.current');
+                    if (currentEl) {
+                      currentEl.setAttribute('style', 'text-content: center');
+                      setDot(currentEl);
                     }
                   }
 
+                  if (!createMockLocation || !handleLocationChange) {
+                    setViewport({
+                      ...viewstate,
+                      bearing: Math.random() * 360,
+                      transitionDuration: 1000,
+                      transitionInterpolator: flyToInterpolator,
+                      transitionEasing: easeCubic,
+                    });
+                    return;
+                  }
+
+                  const coordinates = data?.geometry.coordinates || [];
+                  const len = coordinates.length;
+                  const startLocation: [number, number] = (coordinates[
+                    len - 1
+                  ] || [0, 0]) as [number, number];
+                  const endLocation =
+                    len > 0 ? createMockLocation(startLocation) : [0, 0];
+
+                  handleLocationChange(endLocation);
+
+                  const [endLongitude, endLatitude]: [number, number] =
+                    endLocation;
+                  const [startLongitude, startLatitude]: [number, number] =
+                    startLocation;
+
+                  const endLoc = {
+                    longitude: endLongitude,
+                    latitude: endLatitude,
+                  };
+                  const startLoc = {
+                    longitude: startLongitude,
+                    latitude: startLatitude,
+                  };
+
+                  const bearing = calculateBearing(startLoc, endLoc);
+                  setHeading(bearing);
+
                   setViewport({
                     ...viewstate,
-                    bearing: Math.random() * 360,
+                    longitude: endLongitude,
+                    latitude: endLatitude,
+                    bearing,
                     transitionDuration: 1000,
                     transitionInterpolator: flyToInterpolator,
                     transitionEasing: easeCubic,
--- ui/src/pages/Game.tsx
+++ ui/src/pages/Game.tsx
@@ -9,8 +9,10 @@ import {
   Theme,
 } from '@material-ui/core';
 import Map from '../components/Map';
-import HomeIcon from '@material-ui/icons/Home';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { Link as RouterLink } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -26,14 +28,14 @@ const useStyles = makeStyles((theme: Theme) =>
         backgroundColor: theme.palette.grey[200],
       },
     },
-    homeButton: {
+    backButton: {
       minWidth: 40,
       width: 40,
       height: 40,
       borderRadius: '50%',
       position: 'absolute',
       top: 20,
-      right: 20,
+      left: 20,
     },
   })
 );
@@ -42,18 +44,96 @@ const Game = () => {
   const classes = useStyles();
   // const { huntID } = useParams<{ huntID: string }>();
 
+  const [geojson, setGeojson] = useState<
+    GeoJSON.Feature<GeoJSON.LineString, GeoJSON.GeoJsonProperties>
+  >({
+    type: 'Feature',
+    properties: {},
+    geometry: {
+      type: 'LineString',
+      coordinates: [[-77.0547, 38.9024]],
+    },
+  });
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     const coordinates: GeoJSON.Position[] = geojson.geometry.coordinates;
+  //     const len = coordinates.length;
+  //     const nextLocation = createMockLocation(coordinates[len - 1]);
+  //     coordinates.push(nextLocation);
+
+  //     const replacement: GeoJSON.Feature<
+  //       GeoJSON.LineString,
+  //       GeoJSON.GeoJsonProperties
+  //     > = {
+  //       ...geojson,
+  //       ...{ geometry: { type: 'LineString', coordinates } },
+  //     };
+
+  //     setGeojson(Object.assign({}, replacement));
+  //   }, 5000);
+
+  //   setTimeout(() => clearInterval(interval), 60000);
+  // }, []);
+
+  const createMockLocation = (
+    previousLocation: GeoJSON.Position
+  ): GeoJSON.Position => {
+    const [prevLong, prevLat] = previousLocation;
+
+    let isPositive = Boolean(Math.floor(Math.random() * 2));
+    const nextLong = prevLong + (Math.random() / 100) * (isPositive ? 1 : -1);
+
+    isPositive = Boolean(Math.floor(Math.random() * 2));
+    const nextLat = prevLat + (Math.random() / 100) * (isPositive ? 1 : -1);
+
+    return [nextLong, nextLat];
+  };
+
+  const getLocations = (
+    playerID: string,
+    huntID: string
+  ): [number, number][] => {
+    return [];
+  };
+
+  const addLocation = async (
+    playerID: string,
+    huntID: string,
+    location: { longitude: number; latitude: number }
+  ): Promise<void> => {};
+
   return (
     <>
       <Box className={classes.map}>
-        <Map></Map>
+        <Map
+          geojson={geojson}
+          handleLocationChange={(newLocation: GeoJSON.Position) => {
+            const coordinates: GeoJSON.Position[] =
+              geojson.geometry.coordinates;
+            const nextLocation = newLocation;
+            coordinates.push(nextLocation);
+
+            const replacement: GeoJSON.Feature<
+              GeoJSON.LineString,
+              GeoJSON.GeoJsonProperties
+            > = {
+              ...geojson,
+              ...{ geometry: { type: 'LineString', coordinates } },
+            };
+
+            setGeojson(Object.assign({}, replacement));
+          }}
+          createMockLocation={createMockLocation}
+        ></Map>
       </Box>
 
       <Link component={RouterLink} to="/hunts?type=started">
         <Button
           variant="contained"
-          className={classes.mapButton + ' ' + classes.homeButton}
+          className={classes.mapButton + ' ' + classes.backButton}
         >
-          <HomeIcon />
+          <ArrowBackIcon />
         </Button>
       </Link>
     </>
